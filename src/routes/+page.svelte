2c03398dbf6d17d2d68d3dacf1529eb04987fb1d
Hannah Chiou <hc107@wellesley.edu>
2025-03-03 15:14:56 -0500

    Adding files for Lab 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,12 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="home">
+	<img src="images/my-notion-face-transparent.png" alt="A headshot of Hannah Chiou" width="500px">
+	<div class="text-box">
+		<h2>Hello!</h2>
+		<p>Hannah Chiou is a Wellesley College senior majoring in data science. Special interests include film, math, & running. </p>
+	</div>
+</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+    <title>Contact me</title>
+</svelte:head>
+
+<h2>Contact</h2>
+
+<form action="mailto:hchiou@mit.edu" method="GET">
+    <label>
+        Email:
+        <input name="email" type="email" required>
+    </label>    
+    <label>
+        Subject:
+        <input name="subject" type="text" required>
+    </label>    
+    <label>
+        Body:
+        <textarea name="body" required></textarea>
+    </label>
+
+    <button type="submit">Submit</button>
+</form>
