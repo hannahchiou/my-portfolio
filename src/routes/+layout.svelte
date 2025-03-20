cca239012f52dd945971b8a1c9510434c82c35bf
Hannah Chiou <hc107@wellesley.edu>
2025-03-20 12:13:15 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -81,7 +81,7 @@
 <nav>
     {#each pages as p}
         <a href={p.url} 
-            class:current={$page.url.pathname.endsWith('/' + p.url)}        
+            class:current={$page.url.pathname === '/my-portfolio/' + p.url}
             target={p.url.startsWith("http") ? "_blank" : undefined}>
             {p.title}
         </a>
